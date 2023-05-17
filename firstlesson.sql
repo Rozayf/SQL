69d39863cb7b7c42bb5846efdda3004565ed101c
Rosa Förster <91964206+Rozayf@users.noreply.github.com>
2023-05-17 09:33:37 +0200

    Add files via upload

---
+++ firstlesson.sql
@@ -0,0 +1,21 @@
+USE sakila;
+SELECT *
+FROM actor, film , customer;
+
+SELECT title
+FROM film;
+
+SELECT *
+FROM language;
+
+SELECT COUNT(store_ID)
+FROM store;
+
+SELECT COUNT(store_ID)
+FROM staff;
+
+SELECT first_name
+FROM staff;
+
+SELECT *
+FROM language;
