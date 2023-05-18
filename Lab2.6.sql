fa044ff9942cb23803f693f49278f82d5c7b57d6
Rosa Förster <91964206+Rozayf@users.noreply.github.com>
2023-05-18 12:02:42 +0200

    Add files via upload

---
+++ Lab2.6.sql
@@ -0,0 +1,37 @@
+USE sakila;
+
+SELECT DISTINCT last_name
+FROM actor
+ORDER by last_name ASC;
+
+SELECT last_name
+FROM actor
+GROUP BY last_name
+HAVING COUNT(last_name) > 1
+
+SELECT COUNT(rental_id)
+FROM rental
+WHERE staff_id = 1;
+
+SELECT COUNT(rental_id)
+FROM rental
+WHERE staff_id = 2;
+
+SELECT COUNT(rental_id) AS Count_id , staff_id FROM rental
+GROUP BY staff_id;
+
+SELECT COUNT(title) as title_count, release_year FROM film
+GROUP BY release_year;
+
+
+SELECT COUNT(title) as title_count, rating FROM film
+GROUP BY rating;
+
+SELECT COUNT(title) as title_count, round(AVG(length),2) AS avergage_length, rating FROM film
+GROUP BY rating;
+
+SELECT COUNT(title) as title_count, concat(floor(AVG(length)/60),'h', round(AVG(length)%60,0),'m')AS average_duration, rating FROM film
+GROUP BY rating;
+
+SELECT title, length from film
+ORDER BY length ASC;
